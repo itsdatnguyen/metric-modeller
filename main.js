c33e24a2e32dd149bd389c980e36568003806d5e
DirkBFilter <DirkBFilter@users.noreply.github.com>
2018-04-13 10:03:10 -0400

    Dirk's branch (#11)
    * added design modifier and code modifier metrics to site page.
    also added more coding languages and tweaked values for existing ones.
    
    * removed percent change items
    
    * added form control
    
    * added estimated cost
    
    * fix
    
    * another fix
    
    * changed code convention to same as rest of project
    
    * added more features and changed selects to nominal default
    
    * fixed stuff
    
    * fixed postioning
    
    * broken changes
    
    * All subjective data was removed, only left objective mathamatically
    calculatable data. Time calcuated in months. Data is bad it is what we
    where given so what can you do?
    
    * removed and editied metric modler to accomidate costing assignment.

--- main.js
+++ main.js
@@ -1,119 +1,87 @@
 class MetricModeller {
   constructor() {
-    this.data = [
+    this.languageProd = [
       {
-        name: 'Kappa',
-        language: 8,
-        fps: 10,
-        time: 20,
-        kloc: 5
+        name: 'C++',
+        level: 6.0,
+        fpManMo: 14.84854613,  // function points per man per month
+        locFp: 53.0  // lines of code per function point
       },
       {
-        name: 'Javascript',
-        language: 8,
-        fps: 15,
-        time: 15,
-        kloc: 2
+        name: 'C#',
+        level: 6.0,
+        fpManMo: 14.84854613,
+        locFp: 54.0
       },
       {
-        name: 'C',
-        language: 2.5,
-        fps: 12,
-        time: 30,
-        kloc: 8
+        name: 'Objective-C',
+        level: 12.0,
+        fpManMo: 19.42553559,
+        locFp: 27.0
       },
       {
-        name: 'Haskell',
-        language: 8.5,
-        fps: 30,
-        time: 40,
-        kloc: 3
+        name: 'HTML5',
+        level: 22.0,
+        fpManMo: 23.42797153,
+        locFp: 15.0
       },
-    ]
+      {
+        name: 'Java',
+        level: 6.0,
+        fpManMo: 14.84854613,
+        locFp: 53.0
+      },
+      {
+        name: 'SQL',
+        level: 25.0,
+        fpManMo: 24.27208085,
+        locFp: 13.0
+      },
+      {
+        name: 'PHP',
+        level: 5.0,
+        fpManMo: 13.64464042,
+        locFp: 67.0
+      },
+      {
+        name: 'Python',
+        level: 6.0,
+        fpManMo: 14.84854613,
+        locFp: 53.0
+      },
+      {
+        name: 'VB .net',
+        level: 15.0,
+        fpManMo: 20.89899709,
+        locFp: 20.0
+      },
+    ];
+    this.hourlyCost = 318.4;
   }
 
-  getAverageDataset() {
-    // create copy of array
-    let data = this.data.slice();
-    let length = data.length;
-
-    // possibly convoluted way of getting database averages.
-    return {
-      language: data.reduce((d, a) => typeof d === 'object' ? d.language + a.language : d + a.language) / length,
-      fps: data.reduce((d, a) => typeof d === 'object' ? d.fps + a.fps : d + a.fps) / length,
-      time: data.reduce((d, a) => typeof d === 'object' ? d.time + a.time : d + a.time) / length,
-      kloc: data.reduce((d, a) => typeof d === 'object' ? d.kloc + a.kloc : d + a.kloc) / length
-    }
+  calculateCost(formData, months) {
+    let monthlyCost = this.hourlyCost * 8 * 5 * 4;
+    return months * formData['number-programmers'] * monthlyCost;
   }
 
-  getEstimatedTime(formData) {
-    let dataset = this.getAverageDataset();
-
-    let time = dataset.time;
-
-    // language productivity. A higher value means less lines of code written for the same functionality.
-    time = time * dataset.language / formData.language;
-
-    // 1000 lines of code
-    time = time * formData.kloc / dataset.kloc;
-
-    // Function points. More function points means a longer project
-    time = time * formData.fps / dataset.fps;
-
-    // Team experience. A more experienced team will be more productive.
-    time = time / formData.experience;
-
-    // Project complexity. A more complex project will take longer to complete.
-    time = time * formData.complexity;
-
-    // Project testing coverage. A project with more testing code coverage will last much more longer.
-    time = time * formData.testing;
-
-    // Time to create queries and to database or setup database
-    time = time * formData['database-complexity'];
-
-    // Effect of group cohesion on time it takes to finish project
-    time = time * formData['group-cohesion'];
-    
-    // Project Software Reliability. A project with more reliable software will shorten time.
-    time = time / formData.reliability;
-
-    time = time / Math.sqrt(formData['number-programmers']);
-
-    time = time + Math.sqrt(getCommChannels(formData['number-programmers']));
-
-    // Version control. A project with no version control and lots of programmer will have a longer duration.
-    if (formData['version-control'] == 'false') {
-      time = time * Math.sqrt(formData['number-programmers'])
-    }
-
-    return time;
+  calculateLinesOfCode(formData) {
+    let languageProd = this.languageProd[formData.language];
+    return languageProd.locFp * formData.fps;
   }
 
-  getEstimatedCost(estimatedTime, formData) {
-    var employeePay = formData['programmer-pay'];
-    var employeeNumber = formData['number-programmers'];
-    var estimatedCost = employeePay * employeeNumber * estimatedTime;
-    return estimatedCost;
+  calcualteBaseMonths(formData) {
+    let languageProd = this.languageProd[formData.language];
+    return (formData['number-programmers'] * formData.fps) / languageProd.fpManMo;
   }
+
 }
 
 function getFormData() {
   var data = {};
-
   var elementIds = [
-    'kloc',
     'fps',
     'language',
-    'experience',
-    'complexity',
-    'testing',
-    'programmer-pay',
     'number-programmers',
-    'database-complexity',
-    'group-cohesion',
-    'reliability',
-    'version-control',
   ];
 
   for (var id of elementIds) {
@@ -143,15 +111,15 @@ document.addEventListener('DOMContentLoaded', function() {
     event.preventDefault();
 
     var modeller = new MetricModeller();
-
     var data = getFormData();
-    var estimatedTime = modeller.getEstimatedTime(data);
-    var estimatedCost = modeller.getEstimatedCost(estimatedTime, data);
-
+    var totalMonths = modeller.calcualteBaseMonths(data);
+    var linesOfCode = modeller.calculateLinesOfCode(data);
+    var totalCost = modeller.calculateCost(data, totalMonths);
+    
     setOutput({
-      'output-hours': estimatedTime.toFixed(2),
-      'output-cost': estimatedCost.toFixed(2),
-      'output-comm-channels': getCommChannels(data['number-programmers'])
+      'output-hours': totalMonths.toFixed(2),
+      'output-cost': totalCost.toFixed(2)
     });
+    
   });
 });
